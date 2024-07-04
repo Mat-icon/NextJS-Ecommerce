2e2af16ce999796018e7909cf654f7cc56aea9bd
Matthew Ameh <matthewameh007@gmail.com>
2024-07-04 22:30:16 +0100

    fix lenis

--- src/app/(components)/Lenis.jsx
+++ src/app/(components)/Lenis.jsx
@@ -1,33 +1,22 @@
-'use client'
 
-import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
 
-export const useLenis = () => {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const lenis = new Lenis({
-        duration: 2.2,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        direction: 'vertical', // vertical, horizontal
-        gestureDirection: 'vertical', // vertical, horizontal, both
-        smooth: true,
-        smoothTouch: false,
-        touchMultiplier: 2,
-        infinite: false,
-      });
+const lenis = new Lenis({
+  duration: 2.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  direction: 'vertical', // vertical, horizontal
+  gestureDirection: 'vertical', // vertical, horizontal, both
+  smooth: true,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false,
+});
 
-      function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
 
-      requestAnimationFrame(raf);
+requestAnimationFrame(raf);
 
-      return () => {
-        // Cleanup if necessary
-      };
-    }
-  }, []);
-};
-
+export default lenis;
