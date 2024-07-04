f1c04d1fb4d8a12b62b26399c00ec9b9faa3e906
Matthew Ameh <matthewameh007@gmail.com>
2024-07-04 12:55:54 +0100

    added smooth scroll

--- src/app/(components)/Hero.jsx
+++ src/app/(components)/Hero.jsx
@@ -1,44 +1,37 @@
 'use client'
 
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import Display from "./Display";
 import Promo from "./Promo";
+import lenis from "./Lenis";
 import Navbar from "./Navbar";
+import { useEffect, useRef } from "react";
 import Product from "./Product";
 import Footer from "./Footer";
 import MyFbxModel from "./MyFbxModel";
-
-
-let lenis;
-if (typeof window !== 'undefined') {
-  lenis = require("./Lenis").default;
-}
 
 const Hero = () => {
   const scrollContainer = useRef(null);
 
   useEffect(() => {
-    if (lenis && scrollContainer.current) {
+    if (scrollContainer.current) {
       lenis.on('scroll', ({ scroll }) => {
         console.log(scroll);
       });
     }
 
     return () => {
-      if (lenis) {
-        lenis.off('scroll');
-      }
+      lenis.off('scroll');
     };
   }, []);
-
   return (
-    <div className="h-auto bg-slate-50 selection:bg-emerald-400" ref={scrollContainer}>
-      <Navbar />
-      <Display />
-      <MyFbxModel />
-      <Product />
-      <Promo />
-      <Footer />
+    <div className=" h-auto bg-slate-50  selection:bg-emerald-400" ref={scrollContainer}>
+      <Navbar/>
+      <Display/>
+      <MyFbxModel/>
+      <Product/>
+      <Promo/>
+      <Footer/>
     </div>
   );
 };
